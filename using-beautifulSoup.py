dc249b17689e0285fbf30bda4ce43c05e5150e1b
Lohitaksh Verma <lohitakshverma@MacBookPro.lan>
2025-06-30 23:57:38 +0530

    Using BeautifulSoup

---
+++ using-beautifulSoup.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://bbc.com' 
+
+response= requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
